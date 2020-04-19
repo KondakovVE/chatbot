139854d8190eb5294d2284faf590433178ce80a5
Vladimir Kondakov <vladimir.kondakov@me.com>
2020-04-19 23:42:48 +0300

    pip manifest updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'vk_py_telegrambot',         # How you named your package folder (MyLib)
   packages = ['vk_py_telegrambot'],   # Chose the same as "name"
-  version = '0.3.0',      # Start with a small number and increase it with every change you make
+  version = '0.3.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Vladimir Kondakov',                   # Type in your name
